7a27253f1f5f6384c193b1fa80df416b2f4808f0
Simon Kowallik <github@simonkowallik.com>
2019-03-31 13:44:22 +0200

    update travis ci tests

---
+++ tests/test_fileget.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -ev
+
+# test specific setup
+qkviewid=$(<./qkviewid)
+mkdir -p ./fileget
+
+
+# test simple file fetch
+./ihac-fileget $qkviewid /VERSION | grep '^Product:' > /dev/null || exit 1
+
+
+# test directory ./directory-does-not-exist does not exist
+missing_directory=$(./ihac-fileget -o ./directory-does-not-exist/ $qkviewid /config/bigip.conf 2>&1 && exit 1) || \
+    echo "$missing_directory" | grep 'directory-does-not-exist' > /dev/null || exit 1
+
+
+# download multiple files to output directory
+./ihac-fileget --output-directory ./fileget/ $qkviewid /VERSION /config/bigip.conf && \
+    grep 'TMSH-VERSION' ./fileget/config/bigip.conf && \
+    grep '^Product:' ./fileget/VERSION
+
+
+# download multiple files to output directory using wildcards
+./ihac-fileget --output-directory ./fileget/ $qkviewid /config/bigip*.conf /etc/security/* && \
+    [[ $(find ./fileget -type f | wc -l) -gt 2 ]] || exit 1
--- tests/test_setup.sh
+++ tests/test_setup.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
+set -ev
 
-ihac-auth < ./tests/ihac-credentials.txt
+# authenticate to ihealth
+[[ "$(cat ./tests/cred.txt | ./ihac-auth)" == "OK" ]] || exit 1
 
-ihac-qkviewadd ./tests/qkview_12.1.4.tar.gz | egrep -o '([0-9]+)' > ./qkviewid
+# upload qkview
+qkviewid=$(./ihac-qkviewadd ./tests/qkview.tgz | egrep -o '([0-9]+)')
+echo "$qkviewid" > ./qkviewid
+[[ -z $qkviewid ]] && echo "qkviewid empty" && exit 1
+
+# wait for ihealth to finish analysis
+while [[ "$(./ihac-filelist $qkviewid | wc -l)" -eq "0" ]]
+do
+    sleep 10; echo -n '.'
+done
+echo 'done'
